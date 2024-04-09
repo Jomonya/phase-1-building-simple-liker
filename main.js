ae9dd2c3bff0c817368c184ddab128e0c18d97ec
Joy <joynandako1@gmail.com>
2024-04-09 10:54:20 +0300

    Complete lab

--- main.js
+++ main.js
@@ -2,10 +2,33 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
-
-
-
+// Add event listeners to each heart icon
+document.querySelectorAll(".like-glyph").forEach(heart => {
+  heart.addEventListener("click", () => {
+    if (heart.innerText === EMPTY_HEART) {
+      // Heart is empty, try to fill it
+      mimicServerCall().then(() => {
+        // Success: Fill the heart and add the .activated-heart class
+        heart.innerText = FULL_HEART;
+        heart.classList.add("activated-heart");
+      }).catch(error => {
+        // Failure: Show the error message in the modal
+        const modal = document.getElementById("modal");
+        const modalMessage = document.querySelector("#modal .modal-message");
+        modal.classList.remove("hidden");
+        modalMessage.innerText = error;
+        // Hide the modal after 3 seconds
+        setTimeout(() => {
+          modal.classList.add("hidden");
+        }, 3000);
+      });
+    } else {
+      // Heart is full, empty it
+      heart.innerText = EMPTY_HEART;
+      heart.classList.remove("activated-heart");
+    }
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
